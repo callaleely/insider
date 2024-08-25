4e215fb086ea795b496d63ff60465f1b918dc711
이지영 <jiyoung@ijiyeoncbookpro.mynetworksettings.com>
2024-08-24 21:32:28 -0500

    Check the token balance

--- insider.sol
+++ insider.sol
@@ -1,10 +1,19 @@
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-contract User {
-
+// Interface for ERC-20 tokens
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
 }
 
-contract Post {
-
+contract TokenBalanceChecker {
+    
+    // Function to check the balance of a token for a given address
+    function getTokenBalance(address _tokenAddress, address _userAddress) public view returns (uint256) {
+        // Create an instance of the token contract
+        IERC20 token = IERC20(_tokenAddress);
+        
+        // Return the balance of the user address
+        return token.balanceOf(_userAddress);
+    }
 }
-
